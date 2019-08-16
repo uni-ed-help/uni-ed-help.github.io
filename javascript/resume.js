c8cb23c34068c6ee10e862cc81087873215c1e67
uni-ed-help <uniedhelp@gmail.com>
2019-08-16 15:15:40 -0400

    Edited resume 08/16/2019

--- javascript/resume.js
+++ javascript/resume.js
@@ -1,34 +1,47 @@
 function addExtra() {
+  var name = document.getElementById("extactname").value;
   var role = document.getElementById("roleinput").value;
   var details = document.getElementById("extact-detailinput").value
   var list;
+  var divbutton = document.createElement("div");
   var button = document.createElement("button");
-      if (role === '' || details === '') {
-        alert("You must write something!");
-      }
-      else {
-        list = document.getElementById("extactlist");
-        list.appendChild(button);
-      }
-  }
+  var h3 = document.createElement("h3");
 
+  if (role === '' || details === '') {
+    alert("You must write something!");
+  }
+  else {
+    list = document.getElementById("extactlist");
+    list.appendChild(li1);
+  }
+  h3.appendChild(document.createTextNode(name));
   var roletxt = document.createTextNode(role);
   var detailstxt = document.createTextNode(details);
-  button.appendChild(roletxt);
-  button.appendChild(detailstxt);
-  inputValue = "";
+  button.appendChild(h3);
+  button.classList.add("accordion");
+
+  li2.appendChild(roletxt);
+  li2.appendChild(detailstxt);
+  divbutton.appendChild(button);
+  name = "";
+  role = "";
+  details = "";
   var span = document.createElement("SPAN");
   var txt = document.createTextNode("\u00D7");
   span.className = "close";
   span.appendChild(txt);
-  button.appendChild(span);
+  divbutton.appendChild(span);
   for (i = 0; i < close.length; i++) {
-      close[i].onclick = function() {
-          var div = this.parentElement;
-          div.style.display = "none";
-      }
+    close[i].onclick = function() {
+      var div = this.parentElement;
+      div.style.display = "none";
+    }
   }
 }
+
+
+
+
 
 var myNodelist = document.getElementsByTagName("LI");
 var i;
@@ -73,19 +86,31 @@ function newform(list_type) {
             list.appendChild(li);
         }
     }
-    // else if (list_type == "details") {
-    //     inputValue = document.getElementById("detailinput").value;
-    //     console.log ("inputValue" + inputValue)
-    //     if (inputValue === '') {
-    //         alert("You must write something!");
-    //     }
-    //     else {
-    //         list = document.getElementById("extact-detail");
-    //         console.log ("list:" + list);
-    //         list.appendChild(li);
-    //     }
-    // }
+    else if (list_type == "roles") {
+        inputValue = document.getElementById("roleinput").value;
+        console.log ("roleinputValue" + inputValue)
 
+        if (inputValue === '') {
+            alert("You must write something!");
+        }
+        else {
+            list = document.getElementById("extact-role");
+            console.log ("list:" + list);
+            list.appendChild(li);
+        }
+    }
+    else if (list_type == "details") {
+        inputValue = document.getElementById("detailinput").value;
+        console.log ("inputValue" + inputValue)
+        if (inputValue === '') {
+            alert("You must write something!");
+        }
+        else {
+            list = document.getElementById("extact-detail");
+            console.log ("list:" + list);
+            list.appendChild(li);
+        }
+    }
     var t = document.createTextNode(inputValue);
     li.appendChild(t);
     inputValue = "";
