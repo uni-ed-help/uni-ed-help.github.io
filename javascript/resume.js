008f330f8acfa9d4bcaa2de13444e9cbbc0d5beb
uni-ed-help <uniedhelp@gmail.com>
2019-08-19 13:29:55 -0400

    resume builder 17

--- javascript/resume.js
+++ javascript/resume.js
@@ -1,48 +1,74 @@
-var myNodelist = document.getElementsByTagName("LI");
-var i;
-for (i = 0; i < myNodelist.length; i++) {
-    var span = document.createElement("SPAN");
-    var txt = document.createTextNode("\u00D7");
-    span.className = "close";
-    span.appendChild(txt);
-    myNodelist[i].appendChild(span);
-}
+//function accordionClick(){
+//    var acc = document.getElementsByClassName("accordion");
+//    var i;
+//
+//    for (i = 0; i < acc.length; i++) {
+//        acc[i].addEventListener("click", function() {
+//            /* Toggle between adding and removing the "active" class,
+//            to highlight the button that controls the panel */
+//            this.classList.toggle("active");
+//
+//            /* Toggle between hiding and showing the active panel */
+//            var panel = this.nextElementSibling;
+//            if (panel.style.display === "block") {
+//                panel.style.display = "none";
+//            } else {
+//                panel.style.display = "block";
+//            }
+//        });
+//    }
+//}
+//
 
-var close = document.getElementsByClassName("close");
-var i;
-for (i = 0; i < close.length; i++) {
-    close[i].onclick = function() {
-        var div = this.parentElement;
-        div.style.display = "none";
-    }
-}
-
-var list = document.querySelector('ul');
-list.addEventListener('click', function(ev) {
-    if (ev.target.tagName === 'LI') {
-        ev.target.classList.toggle('checked');
-    }
-}, false);
-
-function newClass() {
-    var li = document.createElement("li");
-    var inputValue = document.getElementById("classinput").value;
-    var t = document.createTextNode(inputValue);
-    li.appendChild(t);
-    if (inputValue === '') {
-        alert("You must write something!");
-    } 
+function addExtra() {
+//    document.getElementById("extra").style.display = "none"
+    var name = document.getElementById("extactname").value;
+    var role = document.getElementById("roleinput").value;
+    var details = document.getElementById("extact-detailinput").value
+    var list;
+    var p = document.createElement("p");
+    var ul = document.createElement("ul");
+    var e = document.getElementById("extact-startmonth");
+    var stmonth = e.options[e.selectedIndex].value;
+    var d = document.getElementById("extact-endmonth");
+    var endmonth = d.options[d.selectedIndex].value;
+    var dates = stmonth + " " + document.getElementById("extact-startyear").value + " - ";
+    if (document.getElementById("extact-present").checked) {
+            dates = dates + "Present";
+            }
     else {
-        document.getElementById("classlist").appendChild(li);
+            dates = dates + endmonth + " " + document.getElementById("extact-endyear").value;
+            }
+
+    if (role === '' || details === '' || name === '') {
+        alert("You must write something!");
     }
-    document.getElementById("classinput").value = "";
+    else {
+        list = document.getElementById("extactlist");
+        list.appendChild(p);
+        list.appendChild(ul);
+    }
+    var span = document.createElement("span");
+    var br = document.createElement("BR");
+    var bre = document.createElement("BR");
+    var it = document.createElement("i");
+    var strong = document.createElement("STRONG");
+    strong.innerHTML = name;
+    it.innerHTML = "&nbsp;" + role;
+    span.classList.add("close");
+    span.appendChild(document.createTextNode("\u00D7"));
+        p.setAttribute("class", "tab");
+    p.appendChild(strong);
+    p.appendChild(it);
+    p.appendChild(span);
+    p.appendChild(br);
+    p.appendChild(document.createTextNode(dates));
+    p.appendChild(bre);
+    p.appendChild(document.createTextNode(details));
 
-    var span = document.createElement("SPAN");
-    var txt = document.createTextNode("\u00D7");
-    span.className = "close";
-    span.appendChild(txt);
-    li.appendChild(span);
-
+    name = "";
+    role = "";
+    details = "";
     for (i = 0; i < close.length; i++) {
         close[i].onclick = function() {
             var div = this.parentElement;
@@ -51,3 +77,89 @@ function newClass() {
     }
 }
 
+
+
+var myNodelist = document.getElementsByTagName("LI");
+var i;
+for (i = 0; i < myNodelist.length; i++) {
+        var span = document.createElement("SPAN");
+        var txt = document.createTextNode("\u00D7");
+        span.className = "close";
+        span.appendChild(txt);
+        myNodelist[i].appendChild(span);
+}
+
+var close = document.getElementsByClassName("close");
+var i;
+for (i = 0; i < close.length; i++) {
+        close[i].onclick = function() {
+                console.log (this.parentElement)
+                var div = this.parentElement;
+                div.style.display = "none";
+        }
+}
+
+var list = document.querySelector('ul');
+list.addEventListener('click', function(ev) {
+        if (ev.target.tagName === 'LI') {
+                ev.target.classList.toggle('checked');
+        }
+}, false);
+
+function newform(list_type) {
+        var inputValue;
+        var list;
+        var li = document.createElement("li");
+        console.log("after inputValue");
+        if (list_type == "classes") {
+                inputValue = document.getElementById("classinput").value;
+                console.log ("classinputValue" + inputValue)
+                if (inputValue === '') {
+                        alert("You must write something!");
+                }
+                else {
+                        list = document.getElementById("classlist");
+                        console.log ("list:" + list);
+                        list.appendChild(li);
+                }
+        }
+        else if (list_type == "roles") {
+                inputValue = document.getElementById("roleinput").value;
+                console.log ("roleinputValue" + inputValue)
+
+                if (inputValue === '') {
+                        alert("You must write something!");
+                }
+                else {
+                        list = document.getElementById("extact-role");
+                        console.log ("list:" + list);
+                        list.appendChild(li);
+                }
+        }
+        else if (list_type == "details") {
+                inputValue = document.getElementById("detailinput").value;
+                console.log ("inputValue" + inputValue)
+                if (inputValue === '') {
+                        alert("You must write something!");
+                }
+                else {
+                        list = document.getElementById("extact-detail");
+                        console.log ("list:" + list);
+                        list.appendChild(li);
+                }
+        }
+        var t = document.createTextNode(inputValue);
+        li.appendChild(t);
+        inputValue = "";
+        var span = document.createElement("SPAN");
+        var txt = document.createTextNode("\u00D7");
+        span.className = "close";
+        span.appendChild(txt);
+        li.appendChild(span);
+        for (i = 0; i < close.length; i++) {
+                close[i].onclick = function() {
+                        var div = this.parentElement;
+                        div.style.display = "none";
+                }
+        }
+}
---
+++ javascript/your-resume.js
@@ -0,0 +1,83 @@
+
+function makeResume() {
+//    var name = document.getElementById("name").value;
+//    var address = document.getElementById("address").value;
+//    var city = document.getElementById("city").value;
+//    var address_state = document.getElementById("address-state").value;
+//    var zipcode = document.getElementById("zipcode").value;
+//    var phone = document.getElementById("phone").value;
+//    var email = document.getElementById("email").value;
+//    var hs = document.getElementById("highschool").value;
+//    var hsstart = document.getElementById("hsstart").value;
+//    var hsgrad = document.getElementById("grad-year").value;
+//    var hscity = document.getElementById("hscity").value;
+//    var hsstate = document.getElementById("hs-state").value;
+//    var gpa = document.getElementById("gpa").value;
+//    var gpascale = document.getElementById("gpascale").value;
+//    
+//    
+    var resume = `<center>
+                <b><!--fullname--></b>
+                <br>
+                <!--address-->
+                <br>
+                <!--city-->, <!--state-->, <!--zipcode-->
+                <br>
+                <!--phonenumber--> /
+                <a href = "mailto:<!--emailaddress-->" target="_blank"><!--email--></a>
+            </center>
+            <table class = "education" width = "100%">
+                <tr class="education">
+                    <td class = "education">
+                        EDUCATION
+                    </td>
+                </tr>
+                <tr class = "education">
+                    <td> 
+                         <!--highschool-->
+                        <br>
+                        GPA: <!--gpa-->/ <!--gpascale-->
+                    </td>
+                    <td>
+                        <!--hsstart-->
+                        - 
+                        <!--grad-year-->
+                        <br>
+                        <!--hs-city-->, <!--hs-state-->
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        RELATED COURSEWORK
+                        <br>
+                        Related Coursework
+                        <!--relcourse-->
+                    </td>
+                </tr>
+            </table>`
+//    resume = resume.replace ("<!--fullname-->", name)
+//    resume = resume.replace ("<!--address-->", address)
+//    resume = resume.replace ("<!--city-->", city)
+//    resume = resume.replace ("<!--state-->", address_state)
+//    resume = resume.replace ("<!--zipcode-->", zipcode)
+//    resume = resume.replace ("<!--phonenumber-->", phone)
+//    resume = resume.replace ("<!--emailaddress-->", email)
+//    resume = resume.replace ("<!--email-->", email)
+//    resume = resume.replace ("<!--highschool-->", hs)
+//    resume = resume.replace ("<!--highschool-->", hs)
+//    resume = resume.replace ("<!--gpa-->", gpa)
+//    resume = resume.replace ("<!--gpascale-->", gpascale)
+//    resume = resume.replace ("<!--hs-state-->", hscity)
+//    resume = resume.replace ("<!--hs-city-->", hsstate)
+    var doc = document.getElementById('theResume').contentWindow.document;
+    doc.open();
+    doc.write(resume);
+    doc.close();
+}
+
+function printResume () {
+    var frm = document.getElementById("theResume").contentWindow;
+    frm.focus();// focus on contentWindow is needed on some ie versions
+    frm.print();
+    return false;
+}
